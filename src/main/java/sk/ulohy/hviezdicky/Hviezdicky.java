305b5baeebe5cd81d5762108d3e07f4ef9ae6217
MiskaCim <m.cvanigova@gmail.com>
2022-11-22 14:56:27 +0100

    not final

--- src/main/java/sk/ulohy/hviezdicky/Hviezdicky.java
+++ src/main/java/sk/ulohy/hviezdicky/Hviezdicky.java
@@ -6,60 +6,59 @@ public class Hviezdicky {
 
     static int rows;
     static int col;
+    static String decision;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String decision;
-//        do {
-//            System.out.println("Do you want to print stars? [click C to continue or X to exit]");
-//            decision = scanner.nextLine().toUpperCase();
-//            if (decision != "C") {
-//                System.out.println("Choose again!");
-//
-//            } else {
-//                amountOfStars();
-//                writeOutStars();
-//            }
-//
-//        } while (decision != "X");
-
-        System.out.println("Do you want to print stars? [click C to continue or X to exit]");
-        decision = scanner.nextLine().toUpperCase();
-
-        if ("C".equals(decision)) {
-            amountOfStars();
-            writeOutStars();
-        } else if ("X".equals(decision)) {
-            System.exit(0);
-        } else {
-            System.out.println("Choose again!");
-        }
-
+        run();
     }
 
+    private static void run() {
+        try {
+            System.out.println("Do you want to print stars? [click C to continue or X to exit]");
+            decision = scanner.nextLine().toUpperCase();
 
-
-    private static void writeOutStars() {
-        System.out.println("Figure: ");
-
-        for (int i = 0; i < rows; i++){
-            System.out.print(" * ");
-
-            for (int j = 0; j < col-1; j++){
-                System.out.print(" * ");
+            if ("C".equals(decision)) {
+                stars();
+            } else if ("X".equals(decision)) {
+                System.exit(0);
+            } else {
+                System.out.println("Choose again!");
+                run();
             }
-            System.out.println();
+
+        } catch (Exception e) {
+            System.out.println("Something wrong!");
         }
     }
 
-    private static void amountOfStars() {
+    private static void stars() {
 
-        System.out.println("Enter number of stars in column: ");
-        rows = scanner.nextInt();
+        try {
+            System.out.println("Enter number of stars in column: ");
+            rows = scanner.nextInt();
 
-        System.out.println("Enter number of stars in row: ");
-        col = scanner.nextInt();
+            System.out.println("Enter number of stars in row: ");
+            col = scanner.nextInt();
 
-        System.out.println("Number of rows: " + rows + "\nNumber of columns: " + col + "\n");
+            System.out.println("Number of rows: " + rows + "\nNumber of columns: " + col + "\n");
+
+            System.out.println("Figure: ");
+
+            for (int i = 0; i < rows; i++){
+                System.out.print(" * ");
+
+                for (int j = 0; j < col-1; j++){
+                    System.out.print(" * ");
+                }
+                System.out.println();
+            }
+            run();
+
+        } catch (Exception e) {
+            System.out.println("Something wrong!");
+            run();
+        }
     }
+
 }
