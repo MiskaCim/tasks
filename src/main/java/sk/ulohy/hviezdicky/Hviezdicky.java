f41ee3a76019e44bf2a0d3d26079108c34eb5a90
MiskaCim <m.cvanigova@gmail.com>
2022-11-22 13:58:27 +0100

    added cycle not working correctly

--- src/main/java/sk/ulohy/hviezdicky/Hviezdicky.java
+++ src/main/java/sk/ulohy/hviezdicky/Hviezdicky.java
@@ -6,11 +6,38 @@ public class Hviezdicky {
 
     static int rows;
     static int col;
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        stars();
-        writeOutStars();
+        String decision;
+//        do {
+//            System.out.println("Do you want to print stars? [click C to continue or X to exit]");
+//            decision = scanner.nextLine().toUpperCase();
+//            if (decision != "C") {
+//                System.out.println("Choose again!");
+//
+//            } else {
+//                amountOfStars();
+//                writeOutStars();
+//            }
+//
+//        } while (decision != "X");
+
+        System.out.println("Do you want to print stars? [click C to continue or X to exit]");
+        decision = scanner.nextLine().toUpperCase();
+
+        if ("C".equals(decision)) {
+            amountOfStars();
+            writeOutStars();
+        } else if ("X".equals(decision)) {
+            System.exit(0);
+        } else {
+            System.out.println("Choose again!");
+        }
+
     }
+
+
 
     private static void writeOutStars() {
         System.out.println("Figure: ");
@@ -25,17 +52,13 @@ public class Hviezdicky {
         }
     }
 
-    private static void stars() {
+    private static void amountOfStars() {
 
         System.out.println("Enter number of stars in column: ");
-        Scanner noRows = new Scanner(System.in);
-
-        rows = Integer.parseInt(noRows.nextLine());
+        rows = scanner.nextInt();
 
         System.out.println("Enter number of stars in row: ");
-        Scanner noColumns = new Scanner(System.in);;
-
-        col = Integer.parseInt(noColumns.nextLine());
+        col = scanner.nextInt();
 
         System.out.println("Number of rows: " + rows + "\nNumber of columns: " + col + "\n");
     }
